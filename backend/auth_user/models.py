d8fb7d44c3379b8a5ca747d6a688b5a06695c3c0
dmitriy levykin <dmlegasy@gmail.com>
2026-03-17 19:52:49 +0300

    Refactor: add abstract model

--- backend/auth_user/models.py
+++ backend/auth_user/models.py
@@ -2,31 +2,29 @@ from django.conf import settings
 from django.db import models
 
 
-class TgProfile(models.Model):
+class BaseProfile(models.Model):
     user = models.OneToOneField(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
-        related_name="tg_profile",
+        related_name="%(class)s_profile",
     )
-    tg_id = models.BigIntegerField(unique=True)
     username = models.CharField(max_length=255, unique=True)
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
+
+    class Meta:
+        abstract = True
+
+
+class TgProfile(BaseProfile):
+    tg_id = models.BigIntegerField(unique=True)
 
     def __str__(self):
         return f"TG Profile: {self.tg_id}"
 
 
-class WebProfile(models.Model):
-    user = models.OneToOneField(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE,
-        related_name="web_profile",
-    )
-    username = models.CharField(max_length=255, unique=True)
+class WebProfile(BaseProfile):
     email = models.EmailField(max_length=255, unique=True)
-    first_name = models.CharField(max_length=255)
-    last_name = models.CharField(max_length=255)
 
     def __str__(self):
         return f"Web Profile: {self.username}"
