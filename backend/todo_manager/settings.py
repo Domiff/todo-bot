97d6f2148c706d08ae6115307abc1ff3b026144f
dmitriy levykin <dmlegasy@gmail.com>
2026-03-22 10:33:52 +0300

    Install and configure CORS

--- backend/todo_manager/settings.py
+++ backend/todo_manager/settings.py
@@ -47,11 +47,13 @@ INSTALLED_APPS = [
     "rest_framework_simplejwt",
     "adrf",
     "drf_spectacular",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -198,3 +200,8 @@ LOGGING = {
         "level": "INFO",
     },
 }
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
